90c9eefa0bae81350d2dfc63e5cf54c1fbbeeca6
aceeenn <sentosaacen@gmail.com>
2022-07-18 16:33:53 +0700

    edit ui profile_page

--- lib/pages/home/profile_page.dart
+++ lib/pages/home/profile_page.dart
@@ -6,13 +6,143 @@ import 'package:sepatuku/theme.dart';
 class ProfilePage extends StatelessWidget {
   const ProfilePage({Key? key}) : super(key: key);
 
+  Widget header() {
+    return AppBar(
+      backgroundColor: backgroundColor1,
+      automaticallyImplyLeading: false,
+      elevation: 0,
+      flexibleSpace: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(
+            defaultMargin,
+          ),
+          child: Row(
+            children: [
+              ClipOval(
+                child: Image.asset(
+                  'assets/image_profile.png',
+                  width: 64,
+                ),
+              ),
+              SizedBox(
+                width: 16,
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Halo, Kusnadi',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 24,
+                        fontWeight: semiBold,
+                      ),
+                    ),
+                    Text(
+                      '@kusnadi',
+                      style: subtitleTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Image.asset(
+                'assets/button_exit.png',
+                width: 20,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget menuItem(String text) {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            text,
+            style: secondaryTextStyle.copyWith(
+              fontSize: 13,
+            ),
+          ),
+          Icon(
+            Icons.chevron_right,
+            color: primaryTextColor,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget content() {
+    return Expanded(
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(
+          horizontal: defaultMargin,
+        ),
+        decoration: BoxDecoration(
+          color: backgroundColor3,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              'Account',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: semiBold,
+              ),
+            ),
+            menuItem(
+              'Edit Profile',
+            ),
+            menuItem(
+              'Your Orders',
+            ),
+            menuItem(
+              'Help',
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              'General',
+              style: primaryTextStyle.copyWith(
+                fontSize: 16,
+                fontWeight: semiBold,
+              ),
+            ),
+            menuItem(
+              'Privacy & Policy',
+            ),
+            menuItem(
+              'Term of Service',
+            ),
+            menuItem(
+              'Rate App',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Profile Page',
-        style: primaryTextStyle,
-      ),
+    return Column(
+      children: [
+        header(),
+        content(),
+      ],
     );
   }
 }
